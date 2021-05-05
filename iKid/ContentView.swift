08f631262eaee11bafba7507337d1bd2fca517de
YuYu Madigan <fymadigan@outlook.com>
2021-05-04 19:39:39 -0700

    Initial Commit

---
+++ iKid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iKid
+//
+//  Created by stlp on 5/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
